fc72b14d3492214680ce34a5ea76703851007383
Rodrigo Araujo <rodrigoasilvacc@gmail.com>
2020-10-04 15:36:14 -0300

    New carousel with PageView

---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:carousel_page_view/app/app_module.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+void main() => runApp(ModularApp(module: AppModule()));
+  
